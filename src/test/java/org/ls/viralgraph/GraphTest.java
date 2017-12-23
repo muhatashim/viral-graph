dc251709d6c747f17898fcae1b0aad3994101ca0
Muhatashim <muhatashim@gmail.com>
2017-12-23 15:14:37 -0500

    Added some more example tests

--- src/test/java/org/ls/viralgraph/GraphTest.java
+++ src/test/java/org/ls/viralgraph/GraphTest.java
@@ -2,6 +2,8 @@ package org.ls.viralgraph;
 
 import org.junit.Test;
 
+import java.util.stream.IntStream;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Muhatashim
@@ -11,19 +13,32 @@ import org.junit.Test;
 public class GraphTest {
 
     @Test
-    public void testGraph() throws Exception {
-
-        Node<?, Integer> const1 = param -> 1;
+    public void testSimpleAddition() throws Exception {
+        Node<Integer, Integer> startNode = param -> param;
         Node<Integer, Integer> plus2 = param -> param + 2;
         Node<Integer, String> resultConverter = param -> "The result is: " + param;
         VoidNode<String> voidNode = System.out::println;
 
         Graph graph = new GraphBuilder()
-                .edge(const1,plus2)
-                .edge(plus2,resultConverter)
-                .edge(resultConverter,voidNode)
+                .edge(startNode, plus2)
+                .edge(plus2, resultConverter)
+                .edge(resultConverter, voidNode)
                 .build();
 
-        graph.fire(const1);
+        graph.fire(startNode, 1);
+    }
+
+    @Test
+    public void testSimpleMultithreading() throws Exception {
+        Node<Integer[], Integer[]> startNode = param -> param;
+        VoidNode<Integer[]> printNumbersForSomeReason = param -> IntStream.range(param[0], param[1]).forEach(System.out::println);
+        VoidNode<Integer[]> printNumbersForSomeReason2 = param -> IntStream.range(param[0], param[1]).forEach(System.out::println);
+
+        Graph graph = new GraphBuilder()
+                .edge(startNode, printNumbersForSomeReason)
+                .edge(startNode, printNumbersForSomeReason2)
+                .build();
+
+        graph.fire(startNode, new Integer[]{0, 1000});
     }
 }
